a4b46677e6285947c5d4acdd0e9e130e3d61e7d5
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-08-11 20:12:55 +0530

    todo commits

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+  return (
+    <>
+    <footer className="py-5 mt-3 bg-dark">
+        <div className="container">
+            <p className="m-0 text-center text-white">
+                Copyright <a href="mailTo:nauticalvigneshmenon@gmail.com">©nauticalvigneshmenon@gmail.com</a> 2024
+            </p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
